8bc9c13cc792fedd9d11c970ca681d4139a9af11
Kiu-Q <s201072@hofung.edu.hk>
2022-05-30 17:52:36 +0800

    modified

--- algorithmForPlantingArea/index.py
+++ algorithmForPlantingArea/index.py
@@ -4,13 +4,34 @@ Created on Fri Feb 25 08:52:08 2022
 
 @author: user
 """
+from multiprocessing.sharedctypes import Value
 from checking.temperature import checkT
 from checking.humidity import checkH
 from checking.light import checkL
 from checking.soilMois import checkS
 
-tmep=input("Enter temperature: ")
+def average(list):
+    return sum(list)/len(list)
+
+def worest(list):
+    list=sorted(list)
+    lower=list[0]
+    for Key, Value in mark:
+        if lower==Value:
+            return Key
+
+temp=input("Enter temperature: ")
 humi=input("Enter humidity: ")
 light=input("Enter light intensity: ")
 sMois=input("Enter soil moisture: ")
-checkT(temp)
+tMark=checkT(temp)
+hMrak=checkH(humi)
+lMark=checkL(light)
+sMark=checkS(sMois)
+mark={"temperature":tMark,"humidity":hMrak,"light":lMark,"soilMois":sMark}
+markList=[mark["temperature"], mark["humidity"],mark["light"],mark["soilMois"]]
+lower=worest(markList)
+averMark=average(markList)
+if averMark>49:
+    print("This area is sutiable for planting, mark: "+str(averMark))
+else: print("Area may not sutiable for planting, mark: "+str(averMark)+" (lower than 50).\n You can change this factor to amke a better environment: "+lower)
